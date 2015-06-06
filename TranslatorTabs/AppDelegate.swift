fd33e28eb59cfe3791da6895669cf834bba3812a
Kurt Sang Hyun Lee <breath103@gmail.com>
2015-06-06 18:12:07 +0900

    Initial Commit

---
+++ TranslatorTabs/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TranslatorTabs
+//
+//  Created by 이상현 on 2015. 6. 6..
+//  Copyright (c) 2015년 Kurt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TranslatorTabs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TranslatorTabs
+//
+//  Created by 이상현 on 2015. 6. 6..
+//  Copyright (c) 2015년 Kurt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
